b95a4ee2be378551c24e6792520ae733568f73df
ArchieAtOrigins <archie@originscreative.com>
2017-03-11 17:34:32 -0800

    Added some further event practice code. Next I'll work out the keypress/charCode

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // REMEMBER: USE DOUBLE EQUALS SIGN ON THE CONDITIONAL IF STATEMENT
 
-const listOne	= document.getElementById("list");
-
+const listOne = document.getElementById("list");
+const listTwo = document.getElementById("list2");
+const listThree = document.getElementById("list3")
 
 
 var color = function() {
@@ -14,4 +15,14 @@ var color = function() {
 	}
 }
 
+var charNum = function() {
+	console.log("CharBoooom!");
+}
+
+var changeCase = function() {
+	console.log("justInCase!");
+}
+
 listOne.addEventListener('click', color);
+document.addEventListener('keypress', charNum);
+listThree.addEventListener('mouseover', changeCase);
